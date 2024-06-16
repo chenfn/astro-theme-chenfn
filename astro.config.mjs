7895807c65eee71739c7f26f51f2f36b7e62132d
chenfn <chenfn.com@gmail.com>
2024-06-16 14:37:27 +0800

    fix problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,36 @@
 // import cloudflare from "@astrojs/cloudflare";
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import expressiveCode from 'astro-expressive-code';
-import { defineConfig, passthroughImageService } from 'astro/config';
-import UnoCSS from 'unocss/astro';
-import config from './site.config';
-import { remarkReadingTime } from './src/lib/remarkReadingTime';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import expressiveCode from 'astro-expressive-code'
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
+import config from './site.config'
+import { remarkReadingTime } from './src/lib/remarkReadingTime'
 
 // https://astro.build/config
 export default defineConfig({
   site: config.url,
   prefetch: true,
   markdown: {
-    rehypePlugins: [remarkReadingTime]
+    rehypePlugins: [remarkReadingTime],
   },
-  integrations: [UnoCSS({
-    injectReset: true // or a path to the reset file
-  }),
-  // must before mdx
-  expressiveCode(), mdx(), sitemap({
-    entryLimit: 10000
-  }), (await import('@playform/compress')).default({
-    CSS: true,
-    HTML: true,
-    Image: false,
-    JavaScript: true,
-    SVG: true,
-    Logger: 2
-  })],
-  // image: {
-  //   service: passthroughImageService(),
-  // },
-  // output: "hybrid",
-  // adapter: cloudflare({
-  //   platformProxy: {
-  //     enabled: true,
-  //     persist: true,
-  //   }
-  // })
-});
+  integrations: [
+    UnoCSS({
+      injectReset: true, // or a path to the reset file
+    }),
+    // must before mdx
+    expressiveCode(),
+    mdx(),
+    sitemap({
+      entryLimit: 10000,
+    }),
+    (await import('@playform/compress')).default({
+      CSS: false,
+      HTML: true,
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+      Logger: 2,
+    }),
+  ],
+})
